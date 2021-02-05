c5ef221c909918211e26877428596eb7bbd86e1e
Seth <seth@ba.io>
2021-02-05 08:27:23 -0500

    Don't unjoin tv room

--- replacer.py
+++ replacer.py
@@ -1,17 +1,17 @@
 import soco
 
 def replace_queue_with_album_list(album_list):
-  tv_room = get_tv_room()
-  tv_room.unjoin()
-  tv_room.clear_queue()
+  coordinator = get_tv_room().group.coordinator
+  #tv_room.unjoin()
+  #tv_room.clear_queue()
   for artist, album in album_list:
-    tracklist = tv_room.music_library.get_tracks_for_album(str(artist), str(album))
+    tracklist = coordinator.music_library.get_tracks_for_album(str(artist), str(album))
     if len(tracklist) == 0:
       print("No tracks found for " + artist + " | " + album)
     else:
       print("Adding tracks for " + artist + " | " + album)
-      tv_room.add_multiple_to_queue(tracklist)
-      tv_room.play()
+      coordinator.add_multiple_to_queue(tracklist)
+      coordinator.play()
 
 def get_tv_room():
   players = list(soco.discover())
